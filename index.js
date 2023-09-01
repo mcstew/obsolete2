d7038e3c19a863037750361a2f71e818d2d629ed
Michael Stewart <mstew@me.com>
2023-09-01 14:08:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,16 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
-// Log: Middleware setup
-console.log("Setting up middleware...");
-
 app.use(cors());
 app.use(express.json());
 
-// Log: API endpoint setup
-console.log("Setting up API endpoint...");
-
 app.post('/api/chat', async (req, res) => {
-  const api_key = process.env.openai_api; // Replace with your OpenAI API key
-
-  // Log: Received API call
-  console.log("Received API call to /api/chat");
-
-  const userInput = req.body.userInput;
-
-  // Log: User input
-  console.log("Received user input:", userInput);
-
+  console.log("API endpoint /api/chat hit");  // Log when endpoint is hit
+  const api_key = process.env.openai_api; // Access the OpenAI API key from environment variable
+  
   try {
-    // Log: Making request to OpenAI
-    console.log("Making request to OpenAI...");
-
+    console.log("Trying to send request to OpenAI");  // Log before sending request to OpenAI
+    const userInput = req.body.userInput;
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
@@ -41,26 +27,14 @@ app.post('/api/chat', async (req, res) => {
         },
       }
     );
-
-    // Log: Response from OpenAI
-    console.log("Received response from OpenAI:", response.data);
-
-    // Log: Sending response back to client
-    console.log("Sending response back to client...");
-
+    console.log("Received response from OpenAI:", response.data);  // Log the response from OpenAI
     res.status(200).json({ message: response.data.choices[0].message.content });
   } catch (error) {
-    // Log: Error details
-    console.error("An error occurred:", error);
-
+    console.error("Error occurred: ", error);  // Log any errors
     res.status(500).json({ error: "An error occurred" });
   }
 });
 
-// Log: Server setup
-console.log("Setting up server...");
-
 app.listen(process.env.PORT || 3000, () => {
-  // Log: Server running
-  console.log('Server running on port:', process.env.PORT || 3000);
+  console.log('Server running');
 });
